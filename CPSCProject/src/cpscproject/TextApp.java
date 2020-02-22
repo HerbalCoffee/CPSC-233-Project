0e18ec6cba4202d0fe2e8f0f1850918fb7166643
klarshin <klarshin@localhost>
2020-02-21 23:28:38 -0700

    Added enemy encounters

--- CPSCProject/src/cpscproject/TextApp.java
+++ CPSCProject/src/cpscproject/TextApp.java
@@ -37,6 +37,17 @@ public class TextApp {
                 playerY = spawnPointY;
             }
         }
+        theMap.setPlayer(playerY, playerX);
+        
+        int numEnemies = 0;
+        while(numEnemies < 3){
+            int x = (int)(Math.random()* theMap.mapLayout[0].length);
+            int y = (int)(Math.random()* theMap.mapLayout.length);
+            if(theMap.isValidMove(y, x)){
+                theMap.mapLayout[y][x] = 'E';
+                numEnemies++;
+            }
+        }
 
         //Instantiate the game loop
         boolean run = true;
@@ -45,7 +56,10 @@ public class TextApp {
         int oldPlayerX = -1;
         int oldPlayerY = -1;
         String special = "";
-
+        int playerHealth = 20;
+        int playerDamage = 5;
+        
+        
         do {
 
             // Set the player's location
@@ -61,15 +75,33 @@ public class TextApp {
             System.out.println(special);
 
             if (special.contains("exit")) {
-                System.out.println("Thanks for playing!");
                 run = false;
+            } else if(special.contains("enemy")){
+                int enemyHealth = (int)(Math.random() * 10);
+                int enemyDamage = (int)(Math.random() * 3);
+                while(enemyHealth > 0){
+                    System.out.println("Enemy Health: " + enemyHealth);
+                    System.out.println("Press A to attack!");
+                    direction = control.next();
+                    if(direction.equalsIgnoreCase("a")){
+                        enemyHealth -= playerDamage;
+                        System.out.println("The enemy attacked!");
+                        playerHealth -= enemyDamage;
+                        System.out.println("Player Health: " + playerHealth);
+                        if(playerHealth <= 0){
+                            enemyHealth = 0;
+                            run = false;
+                        }
+                    }
+                }
+                System.out.println("You killed the enemy!");
+                special = doSpecialActions(theMap, playerX, playerY, playerHealth, playerDamage);
             } else {
                 //Change player location based on user input
                 System.out.println("Select a direction: w = up, a = left, s = down, d = right (q to quit)");
                 direction = control.next();
                 switch (direction) {
                     case "q":
-                        System.out.println("Thanks for playing!");
                         run = false;
                         break;
                     case "w":
@@ -104,25 +136,30 @@ public class TextApp {
                         System.out.println("Enter only a w, a, s, d, or q!");
                 }
 
-                special = doSpecialActions(theMap, playerX, playerY);
+                special = doSpecialActions(theMap, playerX, playerY, playerHealth, playerDamage);
 
-                theMap.replaceElement(oldPlayerY, oldPlayerX, ' ');
+               
             }
-
+            
+             theMap.replaceElement(oldPlayerY, oldPlayerX, ' ');
+            
         } while (run);
-
+        
+        System.out.println("Thanks for playing!");
     }
 
-    public static String doSpecialActions(Map aMap, int xLoc, int yLoc) {
+    public static String doSpecialActions(Map aMap, int xLoc, int yLoc, int playHealth, int playDamage) {
         char currInLoc = aMap.getElement(yLoc, xLoc);
         if (currInLoc == 'I') {
+            playDamage += 2;
             return "You picked up an Iron Sword!";
         }
         if (currInLoc == 'H') {
+            playHealth += 5;
             return "You picked up a health potion!";
         }
         if (currInLoc == 'E') {
-            return "You killed an enemy!";
+            return "You encountered an enemy!";
         }
         if (currInLoc == 'C') {
             return "You reached the exit";
