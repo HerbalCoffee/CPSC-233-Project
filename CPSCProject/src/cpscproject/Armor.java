3ffaec9d7d90855004608352bd04d8cd68b0ba38
HerbalCoffee <59849455+HerbalCoffee@users.noreply.github.com>
2020-02-26 14:33:58 -0700

    Added Three Collectible Sub-Classes

---
+++ CPSCProject/src/cpscproject/Armor.java
@@ -0,0 +1,12 @@
+package cpscproject;
+
+public class Armor extends Collectible {
+
+	private double protection;
+	private double weight;
+	private String displayName;
+	
+	public Armor() {
+		
+	}
+}
---
+++ CPSCProject/src/cpscproject/Consumable.java
@@ -0,0 +1,12 @@
+package cpscproject;
+
+public class Consumable {
+
+	private String displayName;
+	private double restoreAmount;
+	private char consumableType;
+	
+	public Consumable() {
+		
+	}
+}
