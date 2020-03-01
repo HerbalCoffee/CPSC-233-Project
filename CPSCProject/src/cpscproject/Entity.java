774e0d31ad0bade5c97521351776411ae0ea1fd9
TinCan958 <60418461+TinCan958@users.noreply.github.com>
2020-02-29 23:55:22 -0700

    Added methods
    Added getX, getY to help with implementation of distanceFrom, which was also added

--- CPSCProject/src/cpscproject/Entity.java
+++ CPSCProject/src/cpscproject/Entity.java
@@ -22,6 +22,14 @@ public class Entity {
 		this.entityLocation.setX(x);
 	}
 	
+	public int getX() {
+		return this.getLocation.getX();
+	}
+	
+	public int getY() {
+		return this.getLocation.getY();
+	}
+	
 	public void setY(int y) {
 		this.entityLocation.setY(y);
 	}
@@ -30,5 +38,18 @@ public class Entity {
 		return this.entityAsChar;
 	}
 	
+	/**
+	 * Calculates the distance between this entity and otherEntity, using the formula d(P, Q) = √((x2 − x1)^2 + (y2 − y1)^2)
+	 *
+	 */
+
+	public double distanceFrom(Entity otherEntity) {
+		int x1 = this.getX();
+		int y1 = this.getY();
+		int x2 = otherEntity.getX();
+		int y2 = otherEntity.getY();
+		double distance = Math.sqrt((Math.pow(((double)(x2 - x1)), 2)) + (Math.pow(((double)(y2 - y1)), 2)));
+		return distance;
+	}
 
 }
