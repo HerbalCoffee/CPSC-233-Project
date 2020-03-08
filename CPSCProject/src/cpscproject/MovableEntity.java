9fb5b67b65629c01a6201cdc0345dddcd2aa9cfb
TinCan958 <60418461+TinCan958@users.noreply.github.com>
2020-03-07 19:38:00 -0700

    Overhauled stats

--- CPSCProject/src/cpscproject/MovableEntity.java
+++ CPSCProject/src/cpscproject/MovableEntity.java
@@ -1,39 +1,56 @@
 package cpscproject;
+import java.util.*;
 
 public class MovableEntity extends Entity {
 
     // CAN POSSIBLY PUT THESE IN A HASHTABLE LATER ON
     private double health;
+    private final double baseHealth; // Initial health used to determine additional health gained by endurance
     private double baseDamage; // Base damage 
-    private int endurance; // Health multiplier, damage reduction
-    private int strength; // Melee damage + hit chance, carrying capacity
-    private int dexterity; // Ranged damage + hit chance, dodge chance
+    private double accuracy; // Accuracy, determines if an attack hits or misses
+    private double defense; // Defense stat, reduces damage taken. Determined by equipment or preset enemy type
+    private int endurance; // Health multiplier
+    private int strength; // Ability to use equipment without penalties, carrying capacity
+    private int dexterity; // Hit chance, dodge chance, crit chance percentage
     private int intelligence; // Magic damage + hit chance, xp gain, perception ability
 
     public MovableEntity(double health, double baseDamage, int endurance, int strength,
             int dexterity, int intelligence, Location location, char character) {
         super(location, character);
-        this.health = health;
+        this.baseHealth = health;
+        this.calculateActualHealth();
         this.baseDamage = baseDamage;
         this.endurance = endurance;
         this.strength = strength;
         this.dexterity = dexterity;
         this.intelligence = intelligence;
     }
+    
+    /**
+    * Calculates health using the endurance
+    * 
+    * Increases health by baseHealth * (endurance/baseHealth)
+    */
+    public void calculateActualHealth() {
+        this.setHealth( this.getHealth() + (this.getBaseHealth() * (this.getEndurance()/this.getBaseHealth())) );
+    }
 
     //COMBAT DAMAGE METHODS
     /**
      * Calculates damage for a certain attack type based on appropriate stat
-     * Currently adds .5 to the multiplier per level of stat
+     * Adds .5 to the multiplier per level of intelligence for magic attacks and multiplies physical damage by 2.5 on critical hits.
+     * 
      */
     public double getDamage(String damageType) {
         double damageDealt;
         switch (damageType) {
-            case "melee":
-                damageDealt = this.baseDamage + (this.baseDamage * (this.strength / 2));
-                break;
-            case "ranged":
-                damageDealt = this.baseDamage + (this.baseDamage * (this.dexterity / 2));
+            case "physical":
+                damageDealt = this.baseDamage;
+                Random random = new Random();
+                int chance = random.nextInt(101); // Generates an integer between 0 and 100 (both inclusive) to compare to crit chance
+                if (chance <= this.getDexterity()) {
+                    damageDealt *= 2.5;
+                }
                 break;
             case "magic":
                 damageDealt = this.baseDamage + (this.baseDamage * (this.intelligence / 2));
@@ -46,18 +63,38 @@ public class MovableEntity extends Entity {
 
     /*
     * Calculates damage taken and applies damage taken to health
-    * Reduces damage by subtracting (damage * 0.(endurance)) from the damage taken
+    * Reduces damage by subtracting (damage * 0.(defense)) from the damage taken
     *
      */
     public void takeDamage(double damage) {
         double damageTaken = damage;
-        damageTaken = damage - (damage * (0.1 * this.endurance));
+        damageTaken = damage - (damage * (0.1 * this.defense));
         this.health -= damageTaken;
     }
 
     // GETTERS AND SETTERS
+    public double getBaseDamage() {
+        return this.baseDamage;
+    }
+    
     public void setBaseDamage(double newDamage) {
         this.baseDamage = newDamage;
+    }
+    
+    public double getDefense() {
+        return this.defense;
+    }
+    
+    public void setDefense(double newDefense) {
+        this.defense = newDefense;
+    }
+    
+    public double getAccuracy() {
+        return this.accuracy;
+    }
+    
+    public void setAccuracy(double newAccuracy) {
+        this.accuracy = newAccuracy;
     }
 
     public double getHealth() {
@@ -66,6 +103,10 @@ public class MovableEntity extends Entity {
 
     public void setHealth(double health) {
         this.health = health;
+    }
+    
+    public double getBaseHealth() {
+        return this.baseHealth;
     }
 
     public int getEndurance() {
