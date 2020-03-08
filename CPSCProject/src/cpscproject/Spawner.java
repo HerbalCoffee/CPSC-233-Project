df5532ccd7cc9a0d3a339da19c5ac3d9ff3c3641
TinCan958 <60418461+TinCan958@users.noreply.github.com>
2020-03-07 17:24:31 -0700

    Removed repeated code and created a method

--- CPSCProject/src/cpscproject/Spawner.java
+++ CPSCProject/src/cpscproject/Spawner.java
@@ -1,20 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cpscproject;
 
-/**
- *
- * @author ksl
- */
 public class Spawner {
+    
+    public static Location randomLocation() {
+         return new Location((int) (Math.random() * aMap.mapLayout[0].length), (int) (Math.random() * aMap.mapLayout.length));
+    }
     
     public static void spawnEnemies(Map aMap, int numOfEnemies){
         int numEnemies = 0;
         while (numEnemies < numOfEnemies) {
-            Location aLocation = new Location((int) (Math.random() * aMap.mapLayout[0].length), (int) (Math.random() * aMap.mapLayout.length));
+            Location aLocation = Spawner.randomLocation();
             if (aMap.getElement(aLocation) == null) {
                 Enemy theEnemy = new Enemy( (int) (Math.random() * 20) + 1, (int) (Math.random() * 10), new Location(aLocation));
                 //Add each enemy into the map
@@ -28,7 +23,7 @@ public class Spawner {
     public static Player spawnPlayer(Map aMap){
         boolean spawned = false;
         while (!spawned) {
-            Location aLocation = new Location((int) (Math.random() * aMap.mapLayout[0].length), (int) (Math.random() * aMap.mapLayout.length));
+            Location aLocation = Spawner.randomLocation();
             if (aMap.getElement(aLocation) == null) {
                 spawned = true;
                 //Create a new player instance in the spawned location
@@ -41,7 +36,7 @@ public class Spawner {
     public static void spawnConsumable(Map aMap, int numOfItems){
         int numItems = 0;
         while(numItems < numOfItems){
-            Location aLocation = new Location((int) (Math.random() * aMap.mapLayout[0].length), (int) (Math.random() * aMap.mapLayout.length));
+            Location aLocation = Spawner.randomLocation();
             if (aMap.getElement(aLocation) == null) {
                 Consumable newConsumable = new Consumable(new Location(aLocation),'H',"Health Potion",(Math.random()*((30-10)+1))+10);
                 aMap.replaceElement(newConsumable.getLocation(),newConsumable);
@@ -53,7 +48,7 @@ public class Spawner {
     public static void spawnWeapon(Map aMap, int numOfItems){
         int numItems = 0;
         while(numItems < numOfItems){
-            Location aLocation = new Location((int) (Math.random() * aMap.mapLayout[0].length), (int) (Math.random() * aMap.mapLayout.length));
+            Location aLocation = Spawner.randomLocation();
             if (aMap.getElement(aLocation) == null) {
                 Weapon newWeapon = new Weapon(new Location(aLocation),'I',"Iron Sword",((Math.random()*((30-10)+1))+10),0.0);
                 aMap.replaceElement(newWeapon.getLocation(),newWeapon);
